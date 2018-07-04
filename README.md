1a8673c3cb845d15bf79ba327b5c69170e278488
Timothée Gauthier <t.gauthier42@gmail.com>
2018-07-04 23:11:38 +0200

    It works. End of demo
    Don't look at the code too seriously, it was codded really fast.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# wiztivi-hackintime-fika
+# Fika
+
+# Aproject made for wiztivi-hackintime
+
 Project by Team Roquette; https://twitter.com/hashtag/WTVsHackinTime
 
 # installation
@@ -10,3 +13,46 @@ npm install
 npm start
 Then open your browser at  http://127.0.0.1:8080
 ```
+
+# Demo
+
+This project must be run during the 'Fika' presentation to help audience to
+visualize the world we propose
+
+
+## Screen 1 - two portal - Multi Single
+
+Press `f` to go fullScreen for a better experience
+
+On this page you can go around using `arrow keys`
+
+Zoom in : `a`
+Zoom out : `q`
+
+You explain you want to go in 'multiplayer room'
+
+Move screen so you have multiplayer portal on top left of the screen
+
+Then press `a` continuously :
+
+It will zoom inside the portal
+until you see a complete new screen
+
+## Screen 2 - choose a game with friends
+
+You explain your friend are already waiting for you, and you agree on a game
+
+Go on next screen by clicking any where
+
+## Screen 3 - waiting for good people based on profile experience
+
+Nothing to do.
+
+You explain 'we are waiting for the algorithm to find a matching user based on
+his profile experience / mood'
+
+Wait 5 seconds
+
+User appear
+
+End of Demo.
--- game.js
+++ game.js
@@ -17,34 +17,28 @@ var game = new Phaser.Game(
 var speed = 30
 var step = 'portalEntrance'
 
-
-var perso1
-function preload() {
-
-    game.load.image('backdropHome', 'portal.png')
-    game.load.image('backdropWaitGame', 'waitGame.png')
-    game.load.image('backdropWaitPeople', 'waitPeople1.png')
-
-    game.load.image('perso1', 'perso14.png')
-    // game.load.image('pOrange', 'portalOrange.png')
-    game.load.image('pBlue', 'portalBlue.png')
-
-    game.load.image('bluerain', 'bluerain.png')
-    game.load.image('redrain', 'redrain.png')
-    game.load.image('whiterain', 'whiterain.png')
-
-    game.time.advancedTiming = true
-
-}
-
+var character
 var cursors
 var emitterLeft
 var emitterAppear1
 var emitterOrange
 
 var worldScale = minScale
+var i = 1
 
-var spBack
+function preload() {
+    game.load.image('backdropHome', 'choosePortals.png')
+    game.load.image('backdropWaitGame', 'waitGame.png')
+    game.load.image('backdropWaitPeople', 'waitPeople.png')
+
+    game.load.image('character', 'character.png')
+
+    game.load.image('bluerain', 'bluerain.png')
+    game.load.image('redrain', 'redrain.png')
+    game.load.image('whiterain', 'whiterain.png')
+
+    game.time.advancedTiming = true
+}
 
 function create() {
     if (step === 'portalEntrance') {
@@ -55,11 +49,11 @@ function create() {
 function createHome() {
     game.world.setBounds(0, 0, 2000, 2000)
 
-    spBack = game.add.sprite(0, 0, 'backdropHome')
+    game.add.sprite(0, 0, 'backdropHome')
     game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL
 
     cursors = game.input.keyboard.createCursorKeys()
-    zoomKey = game.input.keyboard.addKey(Phaser.KeyCode.Z)
+    zoomKey = game.input.keyboard.addKey(Phaser.KeyCode.F)
     zoomKey.onDown.add(gofull)
 
     emiterYpos = 600
@@ -73,10 +67,6 @@ function createHome() {
     emitterOrange.makeParticles('bluerain', 0, 3000, false, false  )
     emitterOrange.gravity = 0
     emitterOrange.start(false, 1500, 1)
-
-    // TODO DELETE
-    // createSelectAgame()
-    // createWaitPeople()
 }
 
 function createSelectAgame() {
@@ -86,7 +76,6 @@ function createSelectAgame() {
     game.world.pivot.x = 0
     game.world.pivot.y = 0
     speed = 30
-
 
 
     spWaitGame = game.add.sprite(0, 0, 'backdropWaitGame')
@@ -103,18 +92,12 @@ function createUnzoom() {
 function listener() {
     createWaitPeople()
 }
-var tween
-var tween2
 
 function createWaitPeople() {
     // CLEAR
     game.world.removeAll()
     spWaitGame.inputEnabled = false
 
-
-    // create
-    spnull = game.add.sprite(-2000, 0, 'pBlue')
-    spnull.alpha = 1
     game.add.sprite(0, 0, 'backdropWaitPeople')
 
     const emitterY = 470
@@ -122,21 +105,12 @@ function createWaitPeople() {
     const sizePerso1 = 350
 
     // appear people
-    perso1 = game.add.sprite(emitterX - sizePerso1 / 2, emitterY, 'perso1')
-    perso1.anchor.setTo(0, 0)
-    perso1.alpha = 0
-    // cropRect = new Phaser.Rectangle(0, 0, perso1.width, perso1.height)
-    // cropRect.fill('#FF0000')
-    // var tween = game.add.tween(cropRect).to( { height: perso1.height }, 3000, Phaser.Easing.Linear.None, true);
-    // perso1.crop(cropRect)
-    tween2 = game.add.tween(spnull).to( { alpha: 0 }, 6000, Phaser.Easing.Linear.None, true)
-    tween2.onComplete.add(startTween1,this)
-    // tween2.start();
-    // tween.pause()
+    character = game.add.sprite(emitterX - sizePerso1 / 2, emitterY, 'character')
+    character.anchor.setTo(0, 0)
+    character.alpha = 0
+    setTimeout(startTween, 6000)
 
-
-
-    emitterAppear1 = game.add.emitter(emitterX, emitterY + perso1.height / 2, 100)
+    emitterAppear1 = game.add.emitter(emitterX, emitterY + character.height / 2, 100)
     emitterAppear1.makeParticles('whiterain', 0, 3000, false, false  )
     emitterAppear1.gravity = 0
     emitterAppear1.minParticleSpeed.setTo(-500, -500)
@@ -148,15 +122,13 @@ function createWaitPeople() {
     step = 'waitPeople'
 }
 
-function stopMyTween() {
-    // emitterAppear1.destroy()
-    var tween = game.add.tween(emitterAppear1).to( { alpha: 0 }, 500, Phaser.Easing.Linear.None, true)
-    // tween.onComplete.add(stopMyTween,this)
+function startTween() {
+    const tween = game.add.tween(character).to( { alpha: 1 }, 3000, Phaser.Easing.Linear.None, true)
+    tween.onComplete.add(stopEmitterWaitPerso, this)
 }
 
-function startTween1() {
-    tween = game.add.tween(perso1).to( { alpha: 1 }, 3000, Phaser.Easing.Linear.None, true)
-    tween.onComplete.add(stopMyTween,this)
+function stopEmitterWaitPerso() {
+    game.add.tween(emitterAppear1).to( { alpha: 0 }, 500, Phaser.Easing.Linear.None, true)
 }
 
 function gofull() {
@@ -174,7 +146,6 @@ function gofull() {
 
 
 
-var i = 1
 function update() {
     if (step === 'portalEntrance') {
         updateFirst()
@@ -262,6 +233,5 @@ function updateUnZoom() {
 }
 
 function updateWaitPeople() {
-    perso1.updateCrop()
-
+    character.updateCrop()
 }
